b64d12be64e04c01a7532828b9fba8c6637e6285
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-17 21:26:04 +0530

    Welcome to the Address Book Program

---
+++ AddressBook1.java
@@ -0,0 +1,21 @@
+package AddressBook;
+
+import java.util.Scanner;
+class AddressBook1{
+    Scanner s = new Scanner(System.in);
+   
+    class Entry{
+        private String first;
+        private String last;
+        private String address;
+        private String email;
+        Entry(String first, String last, String address, String email){
+            this.first = first;
+            this.last = last;
+            this.address = address;
+            this.email = email;
+   
+       }
+   
+    
+    }}
